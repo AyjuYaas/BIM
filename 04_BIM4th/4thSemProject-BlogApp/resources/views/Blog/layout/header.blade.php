b42b0f648a4e521ac97923d16c71ad717a36a9f1
SAYUJYA SATYAL <sayujya57@gmail.com>
2024-05-13 20:35:16 +0545

    Fix Fontawesome broken links

--- 04_BIM4th/4thSemProject-BlogApp/resources/views/Blog/layout/header.blade.php
+++ 04_BIM4th/4thSemProject-BlogApp/resources/views/Blog/layout/header.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@stack('title')</title>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('./css/styles.css') }}" rel="stylesheet">
 </head>
--- 04_BIM4th/4thSemProject-BlogApp/resources/views/layouts/footer.blade.php
+++ 04_BIM4th/4thSemProject-BlogApp/resources/views/layouts/footer.blade.php
@@ -5,7 +5,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
 </script>
-<script src="https://kit.fontawesome.com/5c14808ee7.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/{{ env('FONT_AWESOME_KIT_KEY') }}.js" crossorigin="anonymous"></script>
+<script src="
+https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/js/all.min.js
+"></script>
 </body>
 
 </html>
